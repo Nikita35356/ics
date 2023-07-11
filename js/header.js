c413c7956abe86e1468cb55795a9675fcd0d2faf
Nikita35356 <106546813+Nikita35356@users.noreply.github.com>
2023-07-11 13:34:04 +0500

    Update header.js

---
+++ js/header.js
@@ -0,0 +1,15 @@
+header.innerHTML += `<div class="icon">
+    <span style="position: absolute;
+    top: 10%;
+    left: 10%;
+    color: #f00;">I</span><span style="position: absolute;
+    top: 25%;
+    left: 30%;
+    color: #0f0;">C</span><span style="position: absolute;
+    top: 40%;
+    left: 60%;
+    color: #00f;">S</span>
+</div>
+    <div><a href="../index.html">Главная</a></div>
+`
+
